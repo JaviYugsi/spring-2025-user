93373219bf34dace5f102ddef8c96d586187f548
Oswaldo Javier Yugsi <oswaldo.yugsi@gmail.com>
2025-04-17 17:44:14 +0200

    Implements controllers

--- src/main/java/edu/uoc/epcsd/user/application/rest/DigitalItemRESTController.java
+++ src/main/java/edu/uoc/epcsd/user/application/rest/DigitalItemRESTController.java
@@ -38,40 +38,102 @@ public class DigitalItemRESTController {
     // use the corresponding mapping HTTP request annotation with the parameter: "/{digitalItemId}"
     // and create the method getDigitalItemById(@PathVariable @NotNull Long digitalItemId)
     // which call the corresponding getDigitalItemById method 
-    
+    @GetMapping("/{digitalItemId}")
+    public ResponseEntity<DigitalItem> getDigitalItemById(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("getDigitalItemById");
+        DigitalItem digitalItem = digitalItemService.getDigitalItemById(digitalItemId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(digitalItem);
+    }
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/digitalItemBySession"
     // and create the method findDigitalItemBySession(@RequestParam @NotNull Long digitalSessionId)
-    // which call the corresponding findDigitalItemBySession method 
+    // which call the corresponding findDigitalItemBySession method
+    @GetMapping("/digitalItemBySession")
+    public List<DigitalItem> findDigitalItemBySession(@RequestParam @NotNull Long digitalSessionId) {
+        log.trace("findDigitalItemBySession");
+        return digitalItemService.findDigitalItemBySession(digitalSessionId);
+    }
     
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/addItem"
     // and create the method addDigitalItem(@RequestBody @Valid CreateDigitalItemRequest createDigitalItemRequest)
-    // which call the corresponding addDigitalItemm method 
+    // which call the corresponding addDigitalItemm method
+    @PostMapping
+    public ResponseEntity<Long> addDigitalItem(@RequestBody @Valid CreateDigitalItemRequest createDigitalItemRequest) {
+        log.trace("addDigitalItem");
+        log.trace("Creating digital item " + createDigitalItemRequest);
+        Long digitalItemId = digitalItemService.addDigitalItem(DigitalItem.builder()
+                .digitalSessionId(createDigitalItemRequest.getDigitalSessionId())
+                .description(createDigitalItemRequest.getDescription())
+                .lat(createDigitalItemRequest.getLat())
+                .lon(createDigitalItemRequest.getLon())
+                .link(createDigitalItemRequest.getLink()).build());
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(digitalItemId)
+                .toUri();
+
+        return ResponseEntity.created(uri).body(digitalItemId);
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/updateItem/{digitalItemId}"
     // and create the method updateDigitalItem(@PathVariable @NotNull Long digitalItemId, @RequestBody @Valid CreateDigitalItemRequest updateDigitalItemRequest)
-    // which call the corresponding updateDigitalItem method 
+    // which call the corresponding updateDigitalItem method
+    @PutMapping(" /updateItem/{digitalItemId}")
+    public ResponseEntity<Long> updateDigitalItem(@PathVariable @NotNull Long digitalItemId, @RequestBody @Valid CreateDigitalItemRequest updateDigitalItemRequest) {
+        log.trace("updateDigitalItem");
+        log.trace("Updating digital item " + updateDigitalItemRequest);
+
+        Long updatedDigitalItemId = digitalItemService.updateDigitalItem(digitalItemId, updateDigitalItemRequest.getDescription(), updateDigitalItemRequest.getLink(), updateDigitalItemRequest.getLat(), updateDigitalItemRequest.getLon());
+
+        return ResponseEntity.ok(updatedDigitalItemId);
+    }
+
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/reviewDigitalItem/{digitalItemId}"
     // and create the method setDigitalItemForReview(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding setDigitalItemForReview method     
+    // which call the corresponding setDigitalItemForReview method
+    @PatchMapping("/reviewDigitalItem/{digitalItemId}")
+    public ResponseEntity<Void> setDigitalItemForReview(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("setDigitalItemForReview");
+        digitalItemService.setDigitalItemForReview(digitalItemId);
+        return ResponseEntity.noContent().build();
+    }
     
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/approveDigitalItem/{digitalItemId}"
     // and create the method approvePendingDigitalItem(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding approvePendingDigitalItem method     
+    // which call the corresponding approvePendingDigitalItem method
+    @PatchMapping("/approveDigitalItem/{digitalItemId}")
+    public ResponseEntity<Void> approvePendingDigitalItem(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("approveDigitalItem");
+        digitalItemService.approvePendingDigitalItem(digitalItemId);
+        return ResponseEntity.noContent().build();
+    }
     
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/rejectDigitalItem/{digitalItemId}"
     // and create the method rejectPendingDigitalItem(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding rejectPendingDigitalItem method    
+    // which call the corresponding rejectPendingDigitalItem method
+    @PatchMapping("/rejectDigitalItem/{digitalItemId}")
+    public ResponseEntity<Void> rejectPendingDigitalItem(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("rejectDigitalItem");
+        digitalItemService.rejectPendingDigitalItem(digitalItemId);
+        return ResponseEntity.noContent().build();
+    }
     
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/dropItem/{digitalItemId}"
     // and create the method dropDigitalItem(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding dropDigitalItem method 
+    // which call the corresponding dropDigitalItem method
+    @DeleteMapping("/dropItem/{digitalItemId}")
+    public ResponseEntity<Void> dropDigitalItem(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("dropDigitalItem");
+        digitalItemService.dropDigitalItem(digitalItemId);
+        return ResponseEntity.noContent().build();
+    }
   
 }
--- src/main/java/edu/uoc/epcsd/user/application/rest/DigitalSessionRESTController.java
+++ src/main/java/edu/uoc/epcsd/user/application/rest/DigitalSessionRESTController.java
@@ -37,26 +37,70 @@ public class DigitalSessionRESTController {
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/{digitalSessionId}"
     // and create the method getDigitalSessionById(@PathVariable @NotNull Long digitalSessionId)
-    // which call the corresponding getDigitalSessionById method 
+    // which call the corresponding getDigitalSessionById method
+    @GetMapping("/{digitalSessionId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<DigitalSession> getDigitalSessionById(@PathVariable @NotNull Long digitalSessionId) {
+        log.trace("getDigitalSessionById");
+        return digitalSessionService.getDigitalSessionById(digitalSessionId).map(digitalSession -> ResponseEntity.ok().body(digitalSession))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/digitalByUser"
     // and create the method findDigitalSessionByUser(@RequestParam @NotNull Long userId)
-    // which call the corresponding findDigitalSessionByUser method 
+    // which call the corresponding findDigitalSessionByUser method
+    @GetMapping("/digitalByUser")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DigitalSession> findDigitalSessionByUser(@RequestParam @NotNull Long userId) {
+        log.trace("findDigitalSessionByUser");
+        return digitalSessionService.findDigitalSessionByUser(userId);
+    }
        
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/createDigital"
     // and create the method createDigitalSession(@RequestBody @Valid CreateDigitalSessionRequest createDigitalSessionRequest)
-    // which call the corresponding createDigitalSession method 
+    // which call the corresponding createDigitalSession method
+    @PostMapping("/createDigital")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Long> createDigitalSession(@RequestBody @Valid CreateDigitalSessionRequest createDigitalSessionRequest) {
+        log.trace("createDigitalSession");
+        Long newDigitalSessionId = digitalSessionService.createDigitalSession(DigitalSession.builder()
+                .description(createDigitalSessionRequest.getDescription())
+                .location(createDigitalSessionRequest.getLocation())
+                .link(createDigitalSessionRequest.getLink())
+                .userId(createDigitalSessionRequest.getUserId()).build());
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newDigitalSessionId)
+                .toUri();
+
+        return ResponseEntity.created(uri).body(newDigitalSessionId);
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/updateDigital/{digitalSessionId}"
     // and create the method updateDigitalSession(@PathVariable @NotNull Long digitalSessionId, @RequestBody @Valid CreateDigitalSessionRequest updateDigitalSessionRequest)
-    // which call the corresponding updateDigitalSession method 
+    // which call the corresponding updateDigitalSession method
+    @PutMapping("/updateDigital/{digitalSessionId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Long> updateDigitalSession(@PathVariable @NotNull Long digitalSessionId, @RequestBody @Valid CreateDigitalSessionRequest updateDigitalSessionRequest) {
+        log.trace("updateDigitalSession");
+        Long upDigitalSessionId = digitalSessionService.updateDigitalSession(digitalSessionId, updateDigitalSessionRequest.getDescription(), updateDigitalSessionRequest.getLink(), updateDigitalSessionRequest.getLocation(), updateDigitalSessionRequest.getUserId());
+        return ResponseEntity.ok(upDigitalSessionId);
+
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/removeDigital/{digitalSessionId}"
     // and create the method removeDigitalSession(@PathVariable @NotNull Long digitalSessionId
-    // which call the corresponding removeDigitalSession method 
+    // which call the corresponding removeDigitalSession method
+    @DeleteMapping("/removeDigital/{digitalSessionId}")
+    public ResponseEntity<Void> removeDigitalSession(@PathVariable @NotNull Long digitalSessionId) {
+        log.trace("removeDigitalSession");
+        digitalSessionService.removeDigitalSession(digitalSessionId);
+        return ResponseEntity.noContent().build();
+    }
 
 }
